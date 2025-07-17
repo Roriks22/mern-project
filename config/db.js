c6087f976cef4a51543541d4a1c140a13bb002e4
RomainJ <romain.jacquesson@gmail.com>
2025-07-17 18:11:50 +0200

    création backend mern-project

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// Connexion mongodb
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
