568d7c6728e2b45b0af42aeccdaa6a96f69c6b5d
RomainJ <romain.jacquesson@gmail.com>
2025-07-22 00:05:43 +0200

    creation_connexion

--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Log from "../components/log";
 
 const Profil = () => {
   return (
-    <div>
-      <h1>Hello Profil</h1>
+    <div className="profil-page">
+      <div className="log-container">
+        <Log signin={false} signup={true} />
+        <div className="log-container">
+          <img src="./img/log.svg" alt="log" />
+        </div>
+      </div>
     </div>
   );
 };
