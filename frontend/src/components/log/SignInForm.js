20cc982982b0c2a7ac0a56c3cb7930a26d242e21
RomainJ <romain.jacquesson@gmail.com>
2025-07-22 18:24:51 +0200

    reglage erreur email password

--- frontend/src/components/log/SignInForm.js
+++ frontend/src/components/log/SignInForm.js
@@ -1,10 +1,44 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const SignInForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const handleLogin = (e) => {};
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setEmailError("");
+    setPasswordError("");
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}api/user/login`,
+      withCredentials: true,
+      data: {
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        if (res.data.errors) {
+          setEmailError(res.data.errors.email || "");
+          setPasswordError(res.data.errors.password || "");
+        } else {
+          console.log("Connexion réussie !");
+          window.location.href = "/";
+        }
+      })
+      .catch((err) => {
+        if (err.response && err.response.data.errors) {
+          setEmailError(err.response.data.errors.email || "");
+          setPasswordError(err.response.data.errors.password || "");
+        } else {
+          console.log("Erreur Axios:", err);
+        }
+      });
+  };
   return (
     <form action="" onSubmit={handleLogin} id="sign-up-form">
       <label htmlFor="email">Email</label>
@@ -16,6 +50,7 @@ const SignInForm = () => {
         onChange={(e) => setEmail(e.target.value)}
         value={email}
       />
+      {emailError && <div className="email error">{emailError}</div>}
       <br />
       <label htmlFor="password">Mot de passe</label>
       <br />
@@ -26,6 +61,7 @@ const SignInForm = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
+      {passwordError && <div className="password error"> {passwordError}</div>}
       <input type="submit" value="Se connecter" />
     </form>
   );
