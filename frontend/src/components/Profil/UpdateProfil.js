0d6c55fd716816ae2a3d488c47435bcc3cf6c18f
RomainJ <romain.jacquesson@gmail.com>
2025-07-26 16:08:15 +0200

    traiter_date_creation_utilisateur

--- frontend/src/components/Profil/UpdateProfil.js
+++ frontend/src/components/Profil/UpdateProfil.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import LeftNav from "../LeftNav";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import UploadImg from "./UploadImg";
+import { updateBio } from "../../actions/user.actions";
+import { dateParser } from "../Utils";
 
 const UpdateProfil = () => {
+  const [bio, setBio] = useState("");
+  const [updateForm, setUpdateForm] = useState(false);
   const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleUpdate = () => {
+    dispatch(updateBio(userData._id, bio));
+    setUpdateForm(false);
+  };
 
   return (
     <div className="profil-container">
@@ -20,6 +30,30 @@ const UpdateProfil = () => {
             alt="user-pic"
           />
           <UploadImg />
+        </div>
+        <div className="right-part">
+          <div className="bio-update">
+            <h3>Bio</h3>
+            {updateForm === false && (
+              <>
+                <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
+                <button onClick={() => setUpdateForm(!updateForm)}>
+                  Modifer bio
+                </button>
+              </>
+            )}
+            {updateForm && (
+              <>
+                <testarea
+                  type="text"
+                  dafautValue={userData.bio}
+                  onChange={(e) => setBio(e.target.value)}
+                ></testarea>
+                <button onClick={handleUpdate}>Valider modifications</button>
+              </>
+            )}
+          </div>
+          <h4>Membre depuis le : {dateParser(userData.createdAt)}</h4>
         </div>
       </div>
     </div>
