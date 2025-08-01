214ef964197bcdee82f8af779564d552c6c843a5
RomainJ <romain.jacquesson@gmail.com>
2025-08-01 15:54:15 +0200

    post_formulaire

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import LeftNav from "../components/LeftNav";
 import Thread from "../components/Thread";
+import { UidContext } from "../components/routes/AppContext";
+import NewPostForm from "../components/Post/NewPostForm";
+import Log from "../components/log";
 
 const Home = () => {
+  const uid = useContext(UidContext);
   return (
     <div className="home">
       <LeftNav />
       <div className="main">
+        <div className="home-header">
+          {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
+        </div>
         <Thread />
       </div>
     </div>
